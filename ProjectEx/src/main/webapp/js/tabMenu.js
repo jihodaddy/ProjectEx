284402600ae9ae17190b19f5b52292827918f750
jihodaddy <94466572+jihodaddy@users.noreply.github.com>
2021-12-16 18:01:40 +0900

    Add files via upload

---
+++ ProjectEx/src/main/webapp/js/tabMenu.js
@@ -0,0 +1,31 @@
+/**
+ *  tabMenu.js
+ */
+
+$(function(){
+	var $tabContent = $('#tabContent div');
+	
+	// 첫번째 항목 선택되어 있게 설정
+	$('tab li:first-child').addClass('selectedItem');
+	
+	$('#tab li').on('click', function(){
+		// 클릭한 탭메뉴 항목 인덱스 알아오기
+		var index = $(this).index();
+		
+		// 탭메뉴항목을 선택된 이미지로 변경
+		//모든 탭메뉴 항목에 선택시 적용된 css 효과 제거 하고
+		$('#tab li').removeClass('selectedItem');	
+			
+		//클릭한 항목에만 선택한 css효과 설정
+		$(this).addClass('selectedItem');
+		
+		//content 이미지 변경
+		//content의 모든 div는 숨기고
+		$tabContent.css('display','none');
+		//클릭한 탭메뉴 항목의 index에 해당되는 div만 보임
+		$tabContent.eq(index).css('display','block');
+	});
+	
+	
+	
+});
